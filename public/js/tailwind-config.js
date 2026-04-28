3b501869a4c4566340741f3fa1359f41962762bb
Jerome Kevin Fiel <2310030-1@southernleytestateu.edu.ph>
2026-04-28 11:38:35 +0800

    commit

---
+++ public/js/tailwind-config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: { 
+        sans: ['Poppins', 'Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif']
+      },
+      colors: {
+        ocean: { 
+          50: '#f0f9ff', 
+          100: '#e0f2fe', 
+          200: '#bae6fd', 
+          300: '#7dd3fc', 
+          400: '#38bdf8', 
+          500: '#0ea5e9', 
+          600: '#0284c7', 
+          700: '#0369a1', 
+          800: '#075985', 
+          900: '#0c4a6e' 
+        },
+        sand: { 
+          50: '#fffbeb', 
+          100: '#fef3c7', 
+          200: '#fde68a', 
+          300: '#fcd34d', 
+          400: '#fbbf24', 
+          500: '#f59e0b', 
+          600: '#d97706' 
+        },
+        coral: { 
+          400: '#fb7185', 
+          500: '#f43f5e', 
+          600: '#e11d48' 
+        },
+      }
+    }
+  }
+}
